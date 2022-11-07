99491c6a237ee769f53a75fe6dd130af4ac151bd
Misha Tuesday <mishatuesday@gmail.com>
2022-11-07 16:23:15 -0500

    commit tree 7d16e63f68faba3962e36c095f000031afbac638

---
+++ app/models/show.rb
@@ -0,0 +1,25 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        Show.maximum('rating')
+    end
+    def self.most_popular_show
+        Show.find_by(rating: Show.highest_rating)
+        # so the test description says to return the name
+        # but it really wants you to return the Show instance
+    end
+    def self.lowest_rating
+        Show.minimum('rating')
+    end
+    def self.least_popular_show
+        Show.find_by(rating: Show.lowest_rating)
+    end
+    def self.ratings_sum
+        Show.sum('rating')
+    end
+    def self.popular_shows
+        Show.where('rating > 5')
+    end
+    def self.shows_by_alphabetical_order
+        Show.order(:name)
+    end
+end
